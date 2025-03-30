5532d753ade5b3785dd1843a276daa03ad4cd11b
harshitksinghai <harshitksinghai.official@gmail.com>
2025-03-30 15:25:32 +0530

    first commit

---
+++ src/main/java/com/harshitksinghai/CodeControl_Server/CodeControlServerApplication.java
@@ -0,0 +1,13 @@
+package com.harshitksinghai.CodeControl_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeControlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeControlServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshitksinghai/CodeControl_Server/CodeControlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshitksinghai.CodeControl_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeControlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
